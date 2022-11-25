3daa995a4b8cbc359da43bfbd81194978273812c
Thoriq As <72277295+Muhthoriqas@users.noreply.github.com>
2022-11-26 02:38:00 +0800

    Add Template

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html')
+
+@app.route('/newsfeed')
+def news_feed():
+    return render_template('news_feed.html')
+
+@app.route('/aboutus')
+def about_us():
+    return render_template('about_us.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
